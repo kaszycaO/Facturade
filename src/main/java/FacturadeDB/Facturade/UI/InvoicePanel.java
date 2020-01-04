e8a1c2483eb4b1724997294bd99659edda8d214c
Oliwier Kaszyca <o_kaszyca@interia.pl>
2020-01-04 19:52:22 +0100

    APP + nowe GUI

--- src/main/java/FacturadeDB/Facturade/UI/InvoicePanel.java
+++ src/main/java/FacturadeDB/Facturade/UI/InvoicePanel.java
@@ -12,16 +12,22 @@ public class InvoicePanel extends JPanel {
     private JTextArea _drawInvoiceTotalPrice;
     private JTextArea _drawfinalPrice;
     private ClientPanel _clientPanel;
+    private JPanel _componentsPanel;
+
 
     InvoicePanel() {
 
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(1020,1080));
 
+
+
+
         _clientPanel = new ClientPanel();
 
-        JPanel componentsPanel = new JPanel();
-        componentsPanel.setLayout(new GridLayout(1,5));
+        _componentsPanel = new JPanel();
+        _componentsPanel.setLayout(new GridLayout(1,5));
+        JScrollPane scrollPane = new JScrollPane(_componentsPanel,  ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
         _drawInvoiceLP = new JTextArea("LP \n\n");
         _drawInvoiceName = new JTextArea("Nazwa Produktu \n\n");
@@ -35,11 +41,11 @@ public class InvoicePanel extends JPanel {
         setTextAreaProp(_drawInvoicePrice);
         setTextAreaProp(_drawInvoiceTotalPrice);
 
-        componentsPanel.add(_drawInvoiceLP);
-        componentsPanel.add(_drawInvoiceName);
-        componentsPanel.add(_drawInvoiceQuantity);
-        componentsPanel.add(_drawInvoicePrice);
-        componentsPanel.add(_drawInvoiceTotalPrice);
+        _componentsPanel.add(_drawInvoiceLP);
+        _componentsPanel.add(_drawInvoiceName);
+        _componentsPanel.add(_drawInvoiceQuantity);
+        _componentsPanel.add(_drawInvoicePrice);
+        _componentsPanel.add(_drawInvoiceTotalPrice);
 
         JPanel finalPrice = new JPanel();
         finalPrice.setPreferredSize(new Dimension(1920,100));
@@ -49,7 +55,7 @@ public class InvoicePanel extends JPanel {
         finalPrice.add(_drawfinalPrice);
 
         add(_clientPanel,BorderLayout.NORTH);
-        add(componentsPanel,BorderLayout.CENTER);
+        add(scrollPane,BorderLayout.CENTER);
         add(finalPrice,BorderLayout.PAGE_END);
 
 
@@ -108,5 +114,9 @@ public class InvoicePanel extends JPanel {
 
     public ClientPanel get_clientPanel() {
         return _clientPanel;
+    }
+
+    public JPanel get_componentsPanel() {
+        return _componentsPanel;
     }
 }
